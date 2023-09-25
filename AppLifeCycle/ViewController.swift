2291d369218f737312e9b616ce55c12ba6e3bc9a
Kelvin Ricafort <kelvs.r@outlook.com>
2023-09-25 15:38:16 +0800

    Initial Commit

---
+++ AppLifeCycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppLifeCycle
+//
+//  Created by Kelvin Ricafort on 9/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
